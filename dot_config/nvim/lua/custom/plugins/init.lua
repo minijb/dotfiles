1c62ec7550c852427512f99600ee8c7ff9ed3429
minijb <1414309848@qq.com>
2025-04-22 22:52:12 +0800

    Initial commit

---
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -0,0 +1,24 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+return {
+  {
+    'hedyhli/outline.nvim',
+    config = function()
+      -- Example mapping to toggle outline
+      vim.keymap.set('n', '<leader>o', '<cmd>Outline<CR>', { desc = 'Toggle Outline' })
+
+      require('outline').setup {
+        -- Your setup opts here (leave empty to use defaults)
+      }
+    end,
+  },
+  {
+    'xeluxee/competitest.nvim',
+    dependencies = 'MunifTanjim/nui.nvim',
+    config = function()
+      require('competitest').setup {}
+    end,
+  },
+}
