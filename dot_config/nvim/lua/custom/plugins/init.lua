bcf7a0bda65c87e9b38d9729b716102085b0f5cc
minijb <1414309848@qq.com>
2025-04-23 23:05:38 +0800

    add neovim plugin : neogit

--- dot_config/nvim/lua/custom/plugins/init.lua
+++ dot_config/nvim/lua/custom/plugins/init.lua
@@ -28,4 +28,16 @@ return {
       require('leap').set_default_mappings()
     end,
   },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+      'ibhagwan/fzf-lua', -- optional
+      'echasnovski/mini.pick', -- optional
+    },
+  },
 }
